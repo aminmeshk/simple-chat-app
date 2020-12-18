9d00c26a06a1c63847e5614865a30e19eb7e3c0a
Amin Meshk <amin.meshk@gmail.com>
2020-12-18 22:01:11 +0330

    ⚙ Config TS, ESLint, Prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,16 @@
 module.exports = {
   root: true,
-  extends: '@react-native-community',
+  extends: [
+    '@react-native-community',
+    'prettier',
+    'prettier/@typescript-eslint',
+    'prettier/react',
+  ],
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
+  rules: {
+    'no-unused-vars': 'warn',
+    '@typescript-eslint/no-unused-vars': 'warn',
+    'prettier/prettier': 'off',
+  },
 };
